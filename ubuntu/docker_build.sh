f6bec5ea3972cd17721a9cce2a57cc1983dcf823
Ashwin Sekhar T K <ashwin.sekhar@cavium.com>
2020-01-17 06:43:58 -0800

    Move CP2k to new Dockerfile model
    Also add an experimental version to reduce the size of
    docker image

--- ubuntu/docker_build.sh
+++ ubuntu/docker_build.sh
@@ -1,4 +1,11 @@
-source ./config
+if [ -z "$1" ]; then
+	echo "Usage: docker_build.sh <APP_NAME>"
+	echo "APP_NAME can be anyone from the below list"
+	APPS=`ls apps`
+	echo $APPS
+	exit	
+fi
+APP=$1
 
 function build_component () {
 	local COMPONENT=$1
@@ -13,27 +20,38 @@ function build_component () {
 function build_benchmark () {
 	local COMPONENT=$1
 	local TAG=$2
-	local BUILDARGS="--build-arg name_tag=$TAG"
+	local MINIMIZE=$3
+	if [ "$MINIMIZE" == "YES" ]; then
+		BUILDARGS="--build-arg finalimage=minimalimage"
+	else
+		BUILDARGS="--build-arg finalimage=fullimage"
+	fi
 	CMD="docker build \
 		-f Dockerfile.$APP_NAME.$TOOLCHAIN \
 		-t ubuntu/$TOOLCHAIN/$COMPONENT:$TAG \
-		$BUILDARGS . "
+		$BUILDARGS ."
 	echo $CMD >> build.sh
 	$CMD || exit 
 }
 
+cd apps/$APP || exit
+source ./config
+
 rm -rf build
 mkdir -p build
 cp ../../base/Dockerfile.$TOOLCHAIN build
-cp Dockerfile.hpl.$TOOLCHAIN build
+cp Dockerfile.${APP_NAME}.$TOOLCHAIN build
 cp -r data build
 cd build
 
-build_component base
 build_component utils
-build_component openmpi
-build_component openblas
-build_benchmark $APP_NAME $APP_TAG
+for comp in base $COMPONENTS; do
+	build_component $comp
+	if [ "$MINIMIZE" == "YES" ]; then
+		build_component ${comp}_minimal
+	fi
+done
+build_benchmark $APP_NAME $APP_TAG $MINIMIZE
 
 echo "##########################################################################"
 echo Run the following command to run the docker
