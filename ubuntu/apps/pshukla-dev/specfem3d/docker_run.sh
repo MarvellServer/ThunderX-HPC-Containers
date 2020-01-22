bf127f5a344fcf42f3689aa008b222338c580e80
Parijat Shukla <pshukla@marvell.com>
2020-01-22 09:11:03 -0800

    deleting pshukla-dev dir

--- ubuntu/apps/pshukla-dev/specfem3d/docker_run.sh
+++
@@ -1,3 +0,0 @@
-# marvell-hpc-specfem3d:202001_v1.0 
-#docker run -it --rm --privileged --shm-size=1G marvell-hpc-hpl:202001_v1.0
-docker run -it --rm --privileged --shm-size=1G  marvell-hpc-specfem3d:202001_v1.0 
