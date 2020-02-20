852263730e41fc5e32be6a73fb315c9e091f4c89
Ashwin Sekhar T K <asekhar@marvell.com>
2020-02-20 01:58:52 -0800

    Improve LAMMPS GPU version performance

--- ubuntu/apps/lammps/gpu/data/run.sh
+++ ubuntu/apps/lammps/gpu/data/run.sh
@@ -3,8 +3,8 @@
 cd /docker/run
 
 if [ "$INPUT" == "in.lj" ]; then
-	mpirun -n 2 --bind-to core --map-by core --mca pml ^ucx -x UCX_TLS=sm lmp -k on g 2 -sf kk -pk kokkos neigh full neigh/qeq half newton off neigh/thread off -var x 8 -var y 8 -var z 6 -in in.lj
+	mpirun -n 2 --bind-to socket --map-by socket --mca pml ^ucx -x UCX_TLS=sm lmp -k on g 2 -sf kk -pk kokkos neigh full neigh/qeq half newton off neigh/thread off -var x 8 -var y 8 -var z 6 -in in.lj
 else
-	mpirun -n 2 --bind-to core --map-by core --mca pml ^ucx -x UCX_TLS=sm lmp -k on g 2 -sf kk -pk kokkos neigh half neigh/qeq full newton on neigh/thread off -var x 8 -var y 8 -var z 6 -in in.reaxc.hns
+	mpirun -n 2 --bind-to socket --map-by socket --mca pml ^ucx -x UCX_TLS=sm lmp -k on g 2 -sf kk -pk kokkos neigh half neigh/qeq full newton on neigh/thread off -var x 8 -var y 8 -var z 6 -in in.reaxc.hns
 fi
 
