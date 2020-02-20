def0a810cda548d752dd16bb135dc990d72ddbee
Srikanth Yalavarthi <59573021+syalavarthi@users.noreply.github.com>
2020-02-20 11:13:41 -0800

    Added gcc-9.3.0-glibc-2.30 build configuration under apps/gcc9 Current configuration builds gcc 9.2.0 + glibc 2.30 + cmake ucx + openmpi + openmc Dockefile needs futher clean and changes to remove redundant files from the images

---
+++ ubuntu/apps/gcc9/data/run.sh
@@ -0,0 +1,17 @@
+export OPENMC_CROSS_SECTIONS=/docker/run/DATA/endfb71_hdf5/cross_sections.xml
+
+
+cd /docker/run
+
+
+CROSS_SECTION_FILE_DIR=/docker/run/DATA/endfb71_hdf5
+if [ ! -f "$CROSS_SECTION_FILE_DIR/cross_sections.xml" ]; then
+        echo "$PWD/DATA/endfb71_hdf5/cross_sections.xml is missing !!!"
+        echo "Download this file from https://anl.box.com/shared/static/9igk353zpy8fn9ttvtrqgzvw1vtejoz6.xz and extract tar -xvf //anl.box.com/shared/static/9igk353zpy8fn9ttvtrqgzvw1vtejoz6.xz into a host directory and map it into the container directory $CROSS_SECTION_FILE_DIR
+. Use the -v argument in docker run to make this mapping"
+        echo "Eg: If /dir/containing/cross_section_dir is the path on host then give '-v /dir/containing/cross_section_dir:$CROSS_SECTION_FILE_DIR' as the docker run argument"
+        exit 1
+fi
+
+
+mpirun --report-bindings --map-by core --bind-to core -np 64 openmc -s 4 /docker/run/DATA
