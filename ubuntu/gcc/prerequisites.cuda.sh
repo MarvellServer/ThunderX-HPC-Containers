7b978eab862d0087d72cb2e69cdad7b6874e9e45
Ashwin Sekhar T K <asekhar@marvell.com>
2020-02-23 22:42:31 -0800

    Add cuda_runtime and cuda_devel Dockerfiles
    These Dockerfiles will directly pull from
    nvcr.io/hpc-publisher/armsc19/cuda:10.2-devel-ubuntu18.04
    and will replace the devel/runtime for cuda apps.

--- ubuntu/gcc/prerequisites.cuda.sh
+++
@@ -1,7 +0,0 @@
-#DEBFILE=BUILDDIR/data/cuda-repo-ubuntu1804-10-2-local-10.2.107-435.17.01_1.0-1_arm64.deb
-#
-#if [ ! -f $DEBFILE ]; then
-#	echo "Required file $DEBFILE missing. Exiting !!"
-#	exit 1
-#fi
-#
