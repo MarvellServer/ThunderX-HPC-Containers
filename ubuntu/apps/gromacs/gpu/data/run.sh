448c311f67c798a3c66c393da4ca7099cdc8a685
Ashwin Sekhar T K <asekhar@marvell.com>
2020-02-25 00:14:23 -0800

    Add run scripts for GROMACS GPU container

--- ubuntu/apps/gromacs/gpu/data/run.sh
+++ ubuntu/apps/gromacs/gpu/data/run.sh
@@ -8,8 +8,34 @@ cd adh_dodec
 
 gmx_mpi grompp -f pme_verlet.mdp
 
-export OMP_NUM_THREADS=1
-mpirun -np 64 --mca pml ^ucx --bind-to socket --map-by socket gmx_mpi mdrun -v -pin on -nsteps 5000 -noconfout -g gromacs_output -npme 0 -ntomp $OMP_NUM_THREADS -s topol.tpr
+export OMP_NUM_THREADS=31
+MPI_PROCS=2
+STEPS=5000
+(mpirun --cpu-set 0-31 --bind-to socket --report-bindings -np $MPI_PROCS \
+	gmx_mpi mdrun -v -pin off -nsteps $STEPS \
+	-noconfout -g gromacs_output -npme 1 -ntomp_pme 1 -gpu_id 0 \
+	-ntomp $OMP_NUM_THREADS -s topol.tpr -nb gpu -pme gpu 2>&1) > socket0.log &
+mpirun --cpu-set 32-63 --bind-to socket --report-bindings -np $MPI_PROCS \
+	gmx_mpi mdrun -v -pin off -nsteps $STEPS \
+	-noconfout -g gromacs_output -npme 1 -ntomp_pme 1 -gpu_id 0 \
+	-ntomp $OMP_NUM_THREADS -s topol.tpr -nb gpu -pme gpu 2>&1 | tee socket1.log
+wait
 
+export OMP_NUM_THREADS=16
+MPI_PROCS=4
+mpirun --map-by socket --bind-to socket --report-bindings -np $MPI_PROCS \
+	gmx_mpi mdrun -v -pin off -nsteps $STEPS --gputasks 0011 \
+	-noconfout -g gromacs_output  -npme 1 -ntomp_pme 1 \
+	-ntomp $OMP_NUM_THREADS -s topol.tpr -nb gpu -pme gpu 2>&1 | tee dual_socket.log
 
+echo "###################################################################"
+echo "Performance of single GROMACS instance on both sockets"
+grep "Performance:" -B 4 dual_socket.log
+
+echo "###################################################################"
+echo "Performance of single GROMACS instance on both sockets"
+echo "Performance of Two separate GROMACS instances on each socket"
+grep "Performance:" -B 4 socket*.log
+
+echo "###################################################################"
 
