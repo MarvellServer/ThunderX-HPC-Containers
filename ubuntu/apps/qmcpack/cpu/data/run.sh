ab7e3d9648db421e406b5405f12fc25eda84c522
Ashwin Sekhar T K <asekhar@marvell.com>
2020-02-25 01:54:59 -0800

    QMCPACK GPU Version
    Also updated both CPU/GPU versions to use the dataset used in
    https://ngc.nvidia.com/catalog/containers/hpc:qmcpack . This is
    basically the same dataset, but the input files are not generated,
    instead can be simply downloaded using the script at
    https://gitlab.com/NVHCP/ngc-examples/raw/master/qmcpack/v3.5.0/get_S32.sh

--- ubuntu/apps/qmcpack/cpu/data/run.sh
+++ ubuntu/apps/qmcpack/cpu/data/run.sh
@@ -6,22 +6,16 @@ S=32
 A=$((S * 4))
 E=$((A * 12))
 
-H5FILEDIR=/docker/run/NiO/h5files/
+H5FILEDIR=/docker/run/S32_example
 if [ ! -f "$H5FILEDIR/NiO-fcc-supertwist111-supershift000-S${S}.h5" ]; then
-	echo "$PWD/h5files/NiO-fcc-supertwist111-supershift000-S${S}.h5 is missing !!!"
-	echo "Download this file from https://anl.app.box.com/s/yxz1ic4kxtdtgpva5hcmlom9ixfl3v3c/folder/18160688946 into a host directory and map it into the container directory $H5FILEDIR
-. Use the -v argument in docker run to make this mapping"
-	echo "Eg: If /dir/containing/h5files is the path on host then give '-v /dir/containing/h5files:$H5FILEDIR' as the docker run argument"
+	echo "$H5FILEDIR/NiO-fcc-supertwist111-supershift000-S${S}.h5 is missing !!!"
 	exit 1
 fi
 
-INPUT_XML=$PWD/NiO/dmc-a${A}-e${E}-cpu/NiO-fcc-S${S}-dmc.xml
+cd /docker/run
 
-sed -i "s#../NiO-fcc-super#NiO/h5files/NiO-fcc-super#g" $INPUT_XML
-sed -i "s#../O.xml#NiO/O.xml#g" $INPUT_XML
-sed -i "s#../Ni.opt.xml#NiO/Ni.opt.xml#g" $INPUT_XML
+INPUT_XML=/docker/run/NiO-fcc-S${S}-dmc.xml
 
-sed -i 's#<parameter name="walkers">                1 </parameter>#<parameter name="walkers">                __WALKERS__ </parameter>#g' $INPUT_XML
-
+sed -i "s#/host_pwd/#$PWD#g" $INPUT_XML
 
 mpirun -np 2 --bind-to socket --map-by socket qmcpack $INPUT_XML
