aa9392e030d0035f998360fc85354c975779bd68
Ashwin Sekhar T K <asekhar@marvell.com>
2020-02-26 01:05:30 -0800

    Fix OpenMC build

--- ubuntu/apps/openmc/data/run.sh
+++ ubuntu/apps/openmc/data/run.sh
@@ -7,8 +7,7 @@ cd /docker/run
 CROSS_SECTION_FILE_DIR=/docker/run/DATA/endfb71_hdf5
 if [ ! -f "$CROSS_SECTION_FILE_DIR/cross_sections.xml" ]; then
         echo "$PWD/DATA/endfb71_hdf5/cross_sections.xml is missing !!!"
-        echo "Download this file from https://anl.box.com/shared/static/9igk353zpy8fn9ttvtrqgzvw1vtejoz6.xz and extract tar -xvf //anl.box.com/shared/static/9igk353zpy8fn9ttvtrqgzvw1vtejoz6.xz into a host directory and map it into the container directory $CROSS_SECTION_FILE_DIR
-. Use the -v argument in docker run to make this mapping"
+        echo "Download this file from https://anl.box.com/shared/static/9igk353zpy8fn9ttvtrqgzvw1vtejoz6.xz . Extract into a host directory and map it into the container directory $CROSS_SECTION_FILE_DIR. Use the -v argument in docker run to make this mapping."
         echo "Eg: If /dir/containing/cross_section_dir is the path on host then give '-v /dir/containing/cross_section_dir:$CROSS_SECTION_FILE_DIR' as the docker run argument"
         exit 1
 fi
