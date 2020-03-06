a3b52671359ca1dcd5fdfe4953a219e95d39ea3c
Srikanth Yalavarthi <syalavarthi@marvell.com>
2020-03-06 05:45:06 -0800

    Changes in BigDFT - removed redundant steps from Dockerfile.bigdft - update thunderx2.rc to fetchbash environment variables

--- ubuntu/apps/bigdft/data/run.sh
+++ ubuntu/apps/bigdft/data/run.sh
@@ -6,14 +6,12 @@ cd /docker/run/
 
 # define environment
 NTASKS_PER_SOCKET=32
-export MCA_OPTS="--mca orte_base_help_aggregate 1"
+export MCA_OPTS="--mca orte_base_help_aggregate 1 --mca btl_vader_single_copy_mechanism none"
 export MPI_OPTS="--report-bindings --bind-to core --map-by ppr:$NTASKS_PER_SOCKET:socket"
 export UCX_LOG_LEVEL=FATAL
-export OMPI_MCA_btl_vader_single_copy_mechanism=none
 ulimit -s unlimited
 
 # run
-source /usr/local/bin/bigdftvars.sh
 time mpirun ${MCA_OPTS} ${MPI_OPTS} bigdft
 
 bash
