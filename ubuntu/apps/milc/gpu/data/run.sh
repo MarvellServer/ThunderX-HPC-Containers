15e61829967586bd620b2bda7b62110dfdfd2cfb
Prince Takkar <ptakkar@marvell.com>
2020-03-06 01:29:59 -0800

    Add dockerfile for GPU version of MILC

---
+++ ubuntu/apps/milc/gpu/data/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo $INPUT
+export CUDA_VISIBLE_DEVICES=0
+export OMP_NUM_THREADS=8
+#export QUDA_ENABLE_P2P=1
+#export QUDA_REORDER_LOCATION=GPU
+export QUDA_RESOURCE_PATH=/docker/run/quda_resource
+
+cd /docker/run
+if [ "$INPUT" == "input.medium" ]; then
+        wget --no-check-certificate https://portal.nersc.gov/project/m888/apex/MILC_lattices/36x36x36x72.chklat
+	mpirun --allow-run-as-root -np 2 --mca pml ^ucx --bind-to socket --map-by socket su3_rhmd_hisq -qmp-geom 1 1 1 2 $INPUT
+else
+        wget --no-check-certificate https://portal.nersc.gov/project/m888/apex/MILC_lattices/18x18x18x36.chklat
+	mpirun --allow-run-as-root -np 1 --mca pml ^ucx --bind-to socket --map-by socket su3_rhmd_hisq -qmp-geom 1 1 1 1 $INPUT
+fi
+
